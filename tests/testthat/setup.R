b26ca9d03192d2a60532788bdcb81c3e4eb985c0
Inés Ortega-Fernández <iortega@gradiant.org>
2023-08-28 15:57:04 +0200

    delete all tmp directories

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,30 +1,40 @@
-
 clean_environment <- function() {
-  library(fs)
+  #detect and delete folders with pattern "Rtmp"
+  PCTempDir <- Sys.getenv("TEMP")
+  folders <- dir(PCTempDir, pattern = "Rtmp", full.names = TRUE)
+  unlink(folders, recursive = TRUE, force = TRUE, expand = TRUE)
+
   # Clean up tensorflow-generated files and caches
   python_temp_dir <- dirname(reticulate::py_run_string(
     "import tempfile; x=tempfile.NamedTemporaryFile().name",
     local = TRUE
   )$x)
 
-  if (dir.exists(file.path(python_temp_dir, "__pycache__"))) {
+  rm_files(python_temp_dir)
+
+  unlink(file.path(fs::path_home(), ".cache", "conda"), recursive = TRUE)
+  unlink(file.path(fs::path_home(), ".cache", "pip"), recursive = TRUE)
+  unlink(file.path(fs::path_home(), ".conda"), recursive = TRUE)
+  unlink(file.path(fs::path_home(), ".config", "calibre"), recursive = TRUE)
+  unlink(file.path(fs::path_home(), ".keras"), recursive = TRUE)
+  unlink(file.path(fs::path_home(), ".dbus"), recursive = TRUE)
+
+}
+
+rm_files <- function(location){
+
+  cat(paste("Cleaning up "), location, "\n")
+
+  if (dir.exists(file.path(location, "__pycache__"))) {
     unlink(file.path(python_temp_dir, "__pycache__"), recursive = TRUE, force = TRUE)
   }
 
-  files <- dir_ls(
-    python_temp_dir,
+  files <- fs::dir_ls(
+    location,
     regexp = "__autograph_generated_file|__pycache__"
   )
 
-  file_delete(files)
-
-  unlink(file.path(path_home(), ".cache", "conda"), recursive = TRUE)
-  unlink(file.path(path_home(), ".cache", "pip"), recursive = TRUE)
-  unlink(file.path(path_home(), ".conda"), recursive = TRUE)
-  unlink(file.path(path_home(), ".config", "calibre"), recursive = TRUE)
-  unlink(file.path(path_home(), ".keras"), recursive = TRUE)
-  unlink(file.path(path_home(), ".dbus"), recursive = TRUE)
+  fs::file_delete(files)
 }
 
-library(withr)
 withr::defer(clean_environment())

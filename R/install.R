8a3d2740112f422d58f698b73ca6efe29b5dae23
Ines Ortega <iortega@gradiant.org>
2025-09-26 14:40:37 +0200

    changed docs for install

--- R/install.R
+++ R/install.R
@@ -9,15 +9,15 @@
 #' On Apple Silicon (arm64), installs \code{tensorflow-macos} (and \code{tensorflow-metal})
 #' instead of the standard \code{tensorflow} wheel.
 #'
-#' @param envname character, name of the virtualenv (default "neuralGAM-venv").
+#' @param envname character, name of the virtualenv (default "neuralGAM-env").
 #' @param python_version Optional. Python version to be used (minimum 3.9).
 #'        If \code{NULL}, uses \code{Sys.which('python3')} (must be >= 3.9).
 #' @param force Force installation and creation of virtualenv (default FALSE).
-#' @usage install_neuralGAM(envname = "neuralGAM-venv", python_version = "3.9", force = FALSE)
+#' @usage install_neuralGAM(envname = "neuralGAM-env", python_version = "3.9", force = FALSE)
 #' @return NULL
 #' @export
 #' @importFrom reticulate py_module_available virtualenv_create virtualenv_python use_virtualenv py_config
-install_neuralGAM <- function(envname = "neuralGAM-venv", python_version = "3.9", force = FALSE) {
+install_neuralGAM <- function(envname = "neuralGAM-env", python_version = "3.9", force = FALSE) {
   venv_root <- file.path(tools::R_user_dir("neuralGAM", "cache"), "venv")
   venv_path <- file.path(venv_root, envname)
   if (!dir.exists(venv_root)) dir.create(venv_root, recursive = TRUE, showWarnings = FALSE)
@@ -51,7 +51,7 @@ install_neuralGAM <- function(envname = "neuralGAM-venv", python_version = "3.9"
   invisible(NULL)
 }
 
-.setup_virtualenv <- function(envname = "neuralGAM-venv") {
+.setup_virtualenv <- function(envname = "neuralGAM-env") {
   venv_root <- file.path(tools::R_user_dir("neuralGAM", "cache"), "venv")
   venv_path <- file.path(venv_root, envname)
 
